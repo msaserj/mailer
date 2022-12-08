0bced696fe14c0670688d319559e274533c4db17
msa <serg.ks@gmail.com>
2022-12-08 17:07:50 +0400

    git ignore nodemodules

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
 const port = 3010
-
+import pswd from "./pass"
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
@@ -17,14 +17,14 @@ let transporter = nodemailer.createTransport({
     },
     auth: {
         user: "serg.ks@gmail.com", // generated ethereal user
-        pass: "zajuuolomdisyagh", // generated ethereal password
+        pass: pswd, // generated ethereal password
     },
 });
 
 app.use(cors())
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // parse application/json
 app.use(bodyParser.json())
@@ -35,12 +35,17 @@ app.post('/SendMessage', (req, res) => {
     res.send('Hello World!')
 
     // send mail with defined transport object
-    let info =  transporter.sendMail({
+    let info = transporter.sendMail({
         from: "testSender", // sender address
         to: "serg.ks@gmail.com", // list of receivers
         subject: subj, // Subject line
         text: "Hello world?", // plain text body
-        html: `<div><h1>Hello world?</h1><h3>${name}</h3><h3>${email}</h3><p>${message}</p></div>`, // html body
+        html: `<div>
+                    <h1>Hello world?</h1>
+                    <h3>${name}</h3>
+                    <h3>${email}</h3>
+                    <p>${message}</p>
+                </div>`, // html body
     });
 
 })
